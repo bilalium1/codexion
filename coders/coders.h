a9fa7ddde572195dd4b32d204e0eaf7ace976d81
B11 <169191972+bilalium1@users.noreply.github.com>
2026-04-17 10:48:48 +0000

    added : structres

--- coders/coders.h
+++ coders/coders.h
@@ -6,7 +6,7 @@
 /*   By: blemrabe <blemrabe@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/17 08:31:20 by blemrabe          #+#    #+#             */
-/*   Updated: 2026/04/17 08:44:33 by blemrabe         ###   ########.fr       */
+/*   Updated: 2026/04/17 10:04:36 by blemrabe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,44 @@
 # include <unistd.h>
 # include <pthread.h>
 # include <string.h>
+
+# define NBR_CDRS	0
+# define TT_BRNT	1
+# define TT_CMPL	2
+# define TT_DEBG	3
+# define TT_RFCT	4
+# define REQ_CMP	5
+# define CLDOWN		6
+# define SCH		7
+
+typedef struct s_dongle
+{
+	pthread_mutex_t	mutex;
+	long			available_at;
+}	t_dongle;
+
+typedef struct s_coder
+{
+	int				id;
+	pthread_t		thread;
+	long			last_compile;
+	int				compile_count;
+	t_dongle		*left;
+	t_dongle		*right;
+	struct s_sim	*sim;
+}	t_coder;
+
+typedef struct s_sim
+{
+	int				*data;
+	int				stop;
+
+	pthread_mutex_t	log_mutex;
+	pthread_mutex_t	stop_mutex;
+
+	t_dongle		*dongles;
+	t_coder			*coders;
+}	t_sim;
 
 int	*parser(int ac, char **av);
 
